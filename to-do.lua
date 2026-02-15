47e283181703380eac2bb612e9e4234a9b34983e
attewy <andrewteng79@gmail.com>
2026-02-15 15:06:43 -0500

    Update TODO comments

--- to-do.lua
+++ to-do.lua
@@ -1,12 +1,15 @@
 --[[
 
 TODO:
-MORE LEVELS!!!
+MORE LEVELS!!! Physics is solid now its time for level design!!
 
 Visuals - clouds, background, blast particles, etc.
+Make projectiles appear different based on charge meter
 
 Animations for pulser
 
 If you want to you can add teleportation and death animations
+
+
 
 ]]--
