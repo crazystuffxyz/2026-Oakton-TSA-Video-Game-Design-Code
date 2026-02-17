4d4dc2c3d1f4f49cbc964d3a621a10da079f40df
attewy <andrewteng79@gmail.com>
2026-02-17 15:31:38 -0500

    Uncomment TODO section for level design tasks

--- to-do.lua
+++ to-do.lua
@@ -3,6 +3,12 @@
 TODO:
 MORE LEVELS!!! Physics is solid now its time for level design!!
 
+Jump
+
+Slope code
+
+Levels extend over many sections with saferoom i think
+
 Visuals - clouds, background, blast particles, etc.
 Make projectiles appear different based on charge meter
 
@@ -13,3 +19,4 @@ If you want to you can add teleportation and death animations
 
 
 ]]--
+
